5aa52c40e3eab959f96d294c0b5c100898ebfa0d
noisersup <patekiapl@gmail.com>
2021-03-24 12:11:27 +0100

    Basic pomodoro widget layout

--- src/components/widgets/PomodoroWidget.vue
+++ src/components/widgets/PomodoroWidget.vue
@@ -1,6 +1,9 @@
 <template>
 <div id="widget">
-
+    <div class="clock">
+        <p class="minutes">24</p>
+        <p class="seconds">59</p>
+    </div>
 </div>
 </template>
 
@@ -15,5 +18,24 @@ export default {
 <style scoped>
 #widget{
     background-color: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p{
+    margin: 0;
+}
+.clock{
+    width:200px;
+    /* height: 100px; */
+    /* text-align: center; */
+}
+.minutes{
+    display: inline;
+    font-size: 80px;
+}
+.seconds{
+    display: inline;
+    font-size: 30px;
 }
 </style>
