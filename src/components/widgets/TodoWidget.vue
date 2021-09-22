530d338c764407d7d0a8b5204d832f751533c510
noisersup <patekiapl@gmail.com>
2021-09-22 05:27:18 +0200

    rice to todo widget

--- src/components/widgets/TodoWidget.vue
+++ src/components/widgets/TodoWidget.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="widget">
-    <div>
-      <input type="text" class="todo-input" placeholder="Insert task..." v-model="taskInput" @keyup.enter="addTask">
+    <div class="header">
+      <h2>TODO</h2>
     </div>
+      <div class="wrapper">
+        <div class="input-data">
+          <input type="text" class="todo-input" v-model="taskInput" @keyup.enter="addTask" required>
+          <div class="underline"></div>
+          <label>Task</label>
+        </div>
+      </div>
     <div v-for="task in tasks" :key="task" class="task">
       <p @click="completeTask(task.id,true)">{{task.title}}</p>
       <div class="removeBtn" @click="removeTask(task.id)">
@@ -26,7 +33,7 @@ export default {
     return {
       taskInput: '',
       tasks: ["","","",""],
-      doneTasks: [""]
+      doneTasks: [{'title':"aaa"}]
     }
   },
   methods: {
@@ -97,7 +104,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     #widget{
-        background-color: rgb(144, 243, 98);
+        background: rgb(30,255,0);
+        background: linear-gradient(135deg, rgba(30,255,0,1) 0%, rgba(209,255,210,1) 100%);
         overflow: hidden;
         overflow-y: scroll;
         scrollbar-width: none;
@@ -105,6 +113,17 @@ export default {
     #widget > *{
         height:40px;
         border-bottom: 1px solid black;
+    }
+    .header{
+      border:none !important;
+      height: 50px !important;
+    }
+    .header * {
+      margin: 0;
+    }
+    .header h2 {
+      padding: 15px 0px 0px 15px;
+      text-align: left;
     }
     .task{
         display: flex;
@@ -115,8 +134,75 @@ export default {
     }
     
     .task:hover{
-      background-color: rgb(81, 255, 0);
+      background-color: #fff;
     }
+
+    .wrapper{
+      background: #fff;
+      border: none !important;
+      padding: 20px 10px 5px 10px;
+      box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    }
+
+    .wrapper .input-data{
+      height: 20px;
+      width: 100%;
+      position: relative;
+    }
+
+    .wrapper .input-data input{
+      height: 100%;
+      width: 100%;
+      border: none;
+      font-size: 17px;
+      border-bottom: 2px solid silver;
+    }
+
+    .wrapper .input-data label{
+      position: absolute;
+      bottom: 0px;
+      left:0;
+      color: grey;
+      pointer-events: none;
+      transition: all 0.3s ease;
+    }
+
+    .input-data input:focus ~ label,
+    .input-data input:valid ~ label {
+      transform: translateY(-20px);
+      font-size: 15px;
+    }
+
+
+    .wrapper .input-data .underline{
+      position:absolute;
+      bottom: -4px;
+      left: -1px;
+      height: 2px;
+      width:105%;
+    }
+
+
+    .input-data .underline:before{
+      position:absolute;
+      content: "";
+      height: 100%;
+      background-color: rgb(178, 255, 143);
+      width:100%;
+      transform: scaleX(0);
+      left: 0;
+      transition: transform 0.3s ease;
+    }
+    input:focus{
+      outline-width: 0;
+          outline: none;
+    }
+
+    .input-data input:focus ~ .underline:before,
+    .input-data input:valid ~  .underline:before{
+        transform: scaleX(1);
+    }
+
     .todo-input{
       height:20px !important;
     }
@@ -134,8 +220,10 @@ export default {
         overflow: hidden;
     }
     .done{
-      background-color: gray;
-      
+      background: rgba(128, 126, 126,0.6);
+    }
+    .done p {        
+      text-decoration: line-through;
     }
     .removeBtn:hover{
       background-color: rgb(255, 87, 87);
